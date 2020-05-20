5181ce34777aee4b6d92824d4b172cdbdfa50f8f
Matthew Torrence <matt@torrencefamily.net>
2020-05-20 10:39:35 -0400

    update docs

--- crates/runtime/core_objects/sidebar-items.js
+++ crates/runtime/core_objects/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["BoolObject",""],["CharObject",""],["Dictionary",""],["FloatObject",""],["Function","A user defined function"],["IntObject",""],["List",""],["ListIterator",""],["Set",""],["Slice",""],["SliceIterator",""],["StringObject",""],["Tuple",""],["VoidObject",""]]});
+initSidebarItems({"struct":[["BoolObject",""],["CharObject",""],["Dictionary",""],["FloatObject",""],["Function","A user defined function"],["IntObject",""],["List",""],["ListIterator",""],["Set",""],["Slice",""],["SliceIterator",""],["StringObject",""],["Tuple",""],["UnitObject",""]]});

43aa3bcd1e762c3ba05d58f05e80e37ecf6a5a07
Matthew Torrence <matt@torrencefamily.net>
2020-05-20 16:21:47 -0400

    update doc build script

--- build_docs.sh
+++ build_docs.sh
@@ -10,10 +10,14 @@ cd ..
 
 git worktree add docs gh-pages
 
+rm -rf ./docs/*
+
 cp -r docsource/_build/html/* ./docs/
 
 touch ./docs/.nojekyll
 
 cargo doc --no-deps --workspace
+
+mkdir ./docs/crates
 
 cp -r ./target/doc/* ./docs/crates

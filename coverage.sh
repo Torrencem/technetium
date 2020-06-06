939c724c78445338436c36bcf6ea33a0dc413045
Torrencem <matt@torrencefamily.net>
2020-06-06 18:45:38 -0400

    slightly fix coverage

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,9 @@
+# Run cargo install grcov and install nightly (rustup install nightly) before running this script
+
 cargo clean
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads"
-cargo build
-cargo test
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Cpanic=abort"
+cargo +nightly build
+cargo +nightly test
 grcov ./target/debug -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage
 
